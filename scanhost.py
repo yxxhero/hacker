e35128e8b060b1a66e7f52552904841987b0563a
root <root@bogon.(none)>
2017-01-03 21:34:41 +0800

    change scanhost.py

--- scanhost.py
+++ scanhost.py
@@ -2,23 +2,31 @@
 #coding:utf-8
 import argparse
 import socket
+import threading
+screenLock=threading.Semaphore(value=1)
 def connscan(tgthost,tgtport):
     try:
         connstk=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         connstk.connect((tgthost,int(tgtport)))
         connstk.send('ViolentPython\r\n')
         results=connstk.recv(100)
+        screenLock.acquire()
+        print "scan port "+tgtport
         print "[+]%d/tcp open" %int(tgtport)
         print "[+]"+str(results)+'\r\n'
-        connstk.close()
     except Exception,e:
+        screenLock.acquire()
+        print "scan port "+tgtport
         print "[-]%d/tcp closed\r\n" %int(tgtport)
+    finally:
+        screenLock.release()
+        connstk.close()
 
 def portscan(tgthost,portlist):
     print tgthost
     for port in portlist:
-        connscan(tgthost,port) 
-    
+        t=threading.Thread(target=connscan,args=(tgthost,port))
+        t.start() 
     
 parser = argparse.ArgumentParser(prog='hostscan.py',usage='hostscan.py -H host -p 22^3306')
 parser.add_argument('-H',dest='tgthost',type=str,help="hostname")
